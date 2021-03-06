48bd28f3cd42a9748534b018eeaf349a1724ef91
Maximilian Felix Stucky <maximilian.stucky@web.de>
2021-03-06 13:35:45 +0100

    Anforderung 5 erfüllt

--- src/main/java/dhbw/fowler1/videostore/Customer.java
+++ src/main/java/dhbw/fowler1/videostore/Customer.java
@@ -51,10 +51,56 @@ public class Customer {
 		addFooterLines(statement, totalAmount, frequentRenterPoints);
 		return statement;
 	}
+	
+	public String htmlStatement() {
+		double totalAmount = 0;
+		int frequentRenterPoints = 0;
+		Enumeration rentals = _rentals.elements();
+		String statement = "<H1>Rental Record for " + getName() + "</H1>";
+		while (rentals.hasMoreElements()) {
+			// double thisAmount = 0;
+			Rental eachRental = (Rental) rentals.nextElement();
 
+			// determine amount for each line
+			switch (eachRental.getMovie().getPriceCode()) {
+			case Movie.REGULAR:
+				increaseAmount(2);
+				if (eachRental.getDaysRented() > 2)
+					increaseAmount((eachRental.getDaysRented() - 2) * 1.5);
+				break;
+			case Movie.NEW_RELEASE:
+				increaseAmount(eachRental.getDaysRented() * 3);
+				break;
+			case Movie.CHILDRENS:
+				increaseAmount(1.5);
+				if (eachRental.getDaysRented() > 3)
+					increaseAmount((eachRental.getDaysRented() - 3) * 1.5);
+				break;
+			}
+
+			// add frequent renter points
+			frequentRenterPoints++;
+			// add bonus for a two day new release rental
+			if ((eachRental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && eachRental.getDaysRented() > 1)
+				frequentRenterPoints++;
+
+			// show figures for this rental
+			statement += "<P>\t" + eachRental.getMovie().getTitle() + "\t" + String.valueOf(getAmount()) + "</P>";
+			totalAmount += getAmount();
+		}
+		addFooterLinesForHtml(statement, totalAmount, frequentRenterPoints);
+		return statement;
+	}
+
+	private String addFooterLinesForHtml(String result, double totalAmount, int frequentRenterPoints) {
+		result += "<P>Amount owed is " + String.valueOf(totalAmount) + "</P>";
+		result += "<P>You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points</P>";
+		return result;
+	}
+	
 	private String addFooterLines(String result, double totalAmount, int frequentRenterPoints) {
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points\n";
 		return result;
 	}
 
